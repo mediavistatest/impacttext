50e694eb38df6bf3af055fc9df5b45cddc12fa00
Predrag Stojiljković <predrag.stojiljkovic@leaditsoftware.com>
2015-04-01 17:55:25 +0200

    listing accounts

--- admin/js/controllers.js
+++ admin/js/controllers.js
@@ -1,113 +1,233 @@
+function setPagingDataSliced($scope, data, totalResultsCount) {
+	$scope.gridData = data;
+	$scope.totalServerItems = totalResultsCount;
+	if (!$scope.$$phase) {
+		$scope.$apply();
+	}
+	$scope.gridOptions.selectAll(false);
+}
+
+function generateOrderByField(sortFields, sortOrders) {
+	if (typeof sortFields == 'undefined' || sortFields == null) {
+		sortFields = [];
+	}
+
+	if (typeof sortOrders == 'undefined' || sortOrders == null) {
+		sortOrders = [];
+	}
+
+	var orderBy = '';
+	for (var i in sortFields) {
+		if (sortFields[i] == '') {
+			continue;
+		}
+
+		var sortOrder = typeof sortOrders[i] == 'undefined' || sortOrders[i] == null ? 'asc' : sortOrders[i].toLowerCase();
+		if (orderBy != '') {
+			orderBy += ', ';
+		}
+		orderBy += sortFields[i].toLowerCase() + " " + sortOrder;
+	}
+
+	return orderBy;
+}
+
+function implode(arrayToImplode, delimiter) {
+	var result = '';
+	if (typeof arrayToImplode == 'undefined' || arrayToImplode == null || arrayToImplode.constructor !== Array) {
+		return result;
+	}
+	if (typeof delimiter == 'undefined' || delimiter == null) {
+		delimiter = ',';
+	}
+	for (var i in arrayToImplode) {
+		if (result != '') {
+			result += '' + delimiter;
+		}
+		result += arrayToImplode[i];
+	}
+	return result;
+}
+
 // create the controller and inject Angular's $scope
 superAdmin.controller('mainController', function($scope) {
-    // create a messCompanyID to display in our view
+	// create a messCompanyID to display in our view
 });
 
-
 // Account List
-superAdmin.controller('AccountListCtrl',['$scope', '$cookieStore', '$http', function($scope, $cookieStore, $http) {
-    $scope.mySelections = [];
-    $scope.filterOptions = {
-        filterText: '',
-        filterBy: ''
-        //filterText2: '',
-        //externalFilter: '',
-        //useExternalFilter: true
-    };
-    $scope.totalServerItems = 0;
-    $scope.pagingOptions = {
-        pageSizes: [5, 10, 20],
-        pageSize: 5,
-        currentPage: 1
-    };
-    // sort
-    $scope.sortOptions = {
-        fields: ['accountName'],
-        directions: ['ASC'],
-        useExternalSorting: true
-    };
-
-    $scope.gridOptions = {
-        data: 'accounts',
-        enableSorting: true,
-        useExternalSorting: true,
-        sortInfo: $scope.sortOptions,
-        rowHeight: 35,
-        selectedItems: $scope.mySelections,
-        showSelectionCheckbox: true,
-        multiSelect: true,
-        selectWithCheckboxOnly: true,
-        enablePaging: true,
-        showFooter: false,
-        pagingOptions: $scope.pagingOptions,
-        columnDefs: [{
-            field: 'accountName',
-            displayName: 'Account Name'
-        }, {
-            field: 'companyID',
-            displayName: 'Company ID'
-        }, {
-            field: 'firstName',
-            displayName: 'Name',
-            cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()">{{row.entity.firstName}} {{row.entity.lastName}}</div>'
-        }, {
-            field: 'emailAddress',
-            displayName: 'Email Address'
-        }, {
-            field: '',
-            displayName: '',
-            cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a class="btn"><i class="fa fa-pencil"></i> Manage Account </a></div>'
-        }]
-    };
-
-	$scope.refresh = function() {
-		$http.post(inspiniaAdminNS.wsUrl + "accounts", $.param({
-			apikey : $cookieStore.get('inspinia_auth_token'),
-			sethttp : 1
-		}))
-			//Successful request to the server
-			.success(function(data, status, headers, config) {
-				if (data == null || typeof data.apicode == 'undefined') {
-					//This should never happen
-					alert("Unidentified error occurred when getting account info!");
-					return;
-				}
-				if (data.apicode == 0) {
-					$scope.accounts = data.apidata;
-				} else {
-					alert("Error occured while getting account info!");
-				}
-			})
-			//An error occurred with this request
-			.error(function(data, status, headers, config) {
-				alert("Error occured while getting account info!");
-			});
+superAdmin.controller('AccountListCtrl', ['$scope', '$cookieStore', '$http', function($scope, $cookieStore, $http) {
+	var self = this;
+	$scope.mySelections = [];
+	$scope.filterOptions = {
+		filterText: '',
+		filterBy: 'accountName',
+		useExternalFilter: true
+	};
+	$scope.totalServerItems = 0;
+	$scope.pagingOptions = {
+		pageSizes: [2, 5, 10, 20],
+		pageSize: 5,
+		currentPage: 1
+	};
+	// sort
+	$scope.sortOptions = {
+		fields: ['accountName'],
+		directions: ['ASC'],
+		useExternalSorting: true
 	};
 
-	$scope.refresh();
-}]);
+	$scope.gridOptions = {
+		data: 'gridData',
+		enableSorting: true,
+		useExternalSorting: true,
+		sortInfo: $scope.sortOptions,
+		rowHeight: 35,
+		selectedItems: $scope.mySelections,
+		showSelectionCheckbox: true,
+		multiSelect: true,
+		selectWithCheckboxOnly: true,
+		enablePaging: true,
+		showFooter: true,
+		pagingOptions: $scope.pagingOptions,
+		totalServerItems: 'totalServerItems',
+		primaryKey : 'accountID',
+		columnDefs:
+		[
+			{
+				field: 'accountName',
+				displayName: 'Account Name'
+			},
+			{
+				field: 'companyID',
+				displayName: 'Company ID'
+			},
+			{
+				field: 'firstName',
+				displayName: 'Name',
+				cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()">{{row.entity.firstName}} {{row.entity.lastName}}</div>'
+			},
+			{
+				field: 'emailAddress',
+				displayName: 'Email Address'
+			},
+			{
+				field: '',
+				displayName: '',
+				cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a class="btn"><i class="fa fa-pencil"></i> Manage Account </a></div>'
+			}
+		]
+};
+
+$scope.setPagingDataSliced = setPagingDataSliced;
+
+$scope.$watch('pagingOptions', function(newVal, oldVal) {
+	if (!self.poInit || self.gettingData) {
+		self.poInit = true;
+		return;
+	}
+	if (newVal.currentPage != oldVal.currentPage || newVal.pageSize != oldVal.pageSize) {
+		$scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText, $scope.filterOptions.filterBy, $scope.sortOptions.fields, $scope.sortOptions.directions);
+	}
+}, true);
+
+$scope.$watch('sortOptions', function(newVal, oldVal) {
+	if (!self.soInit || self.gettingData) {
+		self.soInit = true;
+		return;
+	}
+	if (implode(newVal.fields) !== implode(oldVal.fields) || implode(newVal.directions) !== implode(oldVal.directions)) {
+		$scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText, $scope.filterOptions.filterBy, $scope.sortOptions.fields, $scope.sortOptions.directions);
+	}
+}, true);
+
+$scope.getPagedDataAsync = function(pageSize, page, searchText, filterBy, sortFields, sortOrders) {
+	if (typeof page == 'undefined' || page == null || page == '') {
+		page = 0;
+	}
+
+	var orderBy = generateOrderByField(sortFields, sortOrders);
+	if (orderBy == '') {
+		orderBy = "accountName";
+	}
+
+	setTimeout(function() {
+		var data;
+		self.gettingData = true;
+		if (searchText) {
+			var ft = searchText.toLowerCase();
+			$http.post(
+				inspiniaNS.wsUrl + "accounts",
+				$.param({sethttp: 1, apikey: $cookieStore.get('inspinia_auth_token'), accountName: ft, orderby: orderBy, limit: pageSize, offset: (page - 1) * pageSize})
+			).success(
+				function (data) {
+					$scope.setPagingDataSliced($scope, data.apidata, data.apicount);
+					self.gettingData = false;
+				}).error(
+
+				//An error occurred with this request
+				function(data, status, headers, config) {
+					//if (status == 400) {
+					alert("An error occurred when getting account lists! Error code: " + data.apicode);
+					alert(JSON.stringify(data));
+					//}
+					self.gettingData = false;
+				}
+			);
+		} else {
+			$http.post(
+				inspiniaAdminNS.wsUrl + "accounts", $.param({
+					sethttp : 1, apikey : $cookieStore.get('inspinia_auth_token'),
+					limit: pageSize, offset: (page - 1) * pageSize, orderby : orderBy
+				})
+			).success(
+				function (data) {
+					$scope.setPagingDataSliced($scope, data.apidata, data.apicount);
+					self.gettingData = false;
+				}).error(
+				//An error occurred with this request
+				function(data, status, headers, config) {
+					//alert('Unexpected error occurred when trying to fetch contact lists!');
+					//if (status == 422) {
+					alert("An error occurred when getting contact lists! Error code: " + data.apicode);
+					alert(JSON.stringify(data));
+					//}
+					self.gettingData = false;
+				}
+			);
+		}
+	}, 100);
+};
+
+$scope.refresh = function() {
+	$scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText, $scope.filterOptions.filterBy, $scope.sortOptions.fields, $scope.sortOptions.directions);
+	$scope.mySelections.length = 0;
+};
+
+$scope.refresh();
+}])
+;
 
 
 //Manage Account
 superAdmin.controller('ManageAccountCtrl', function($scope, notify) {
-    
-    $scope.AccCode = 'Long';
 
-    $scope.msg = 'Hello! This is a sample message!';
-    $scope.template = '';
+	$scope.AccCode = 'Long';
 
-    $scope.demo = function(){
-        notify({
-            message: $scope.msg,
-            classes: $scope.classes,
-            templateUrl: $scope.template,
-            duration: 3000
-        });
-    };
+	$scope.msg = 'Hello! This is a sample message!';
+	$scope.template = '';
 
-    $scope.closeAll = function(){
-        notify.closeAll();
-    };
+	$scope.demo = function() {
+		notify({
+			message: $scope.msg,
+			classes: $scope.classes,
+			templateUrl: $scope.template,
+			duration: 3000
+		});
+	};
+
+	$scope.closeAll = function() {
+		notify.closeAll();
+	};
 
 });
 
