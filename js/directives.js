b387e628c17d76450a172b6bfcc7b904bc1dbbd3
mediavistatest <mediavistatest@gmail.com>
2015-09-14 16:54:16 +0200

    edits

--- js/directives.js
+++ js/directives.js
@@ -24,9 +24,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'ImpactText | Portal';
+                var title = 'CentralMessage | Portal';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'ImpactText | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title = 'CentralMessage | ' + toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
